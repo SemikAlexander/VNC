ce952a499f27795eccc9eb434883185ed16901e7
SemikAlexander <36263835+SemikAlexander@users.noreply.github.com>
2019-04-14 22:21:28 +0300

    VNC simulator

---
+++ VncClient/VncClient/VncClient/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace VncClient
+{
+    partial class VncClientForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.remoteDesktop1 = new VncSharp.RemoteDesktop();
+            this.hostLabel = new System.Windows.Forms.Label();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.hostTextBox = new System.Windows.Forms.TextBox();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // remoteDesktop1
+            // 
+            this.remoteDesktop1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.remoteDesktop1.AutoScroll = true;
+            this.remoteDesktop1.AutoScrollMinSize = new System.Drawing.Size(608, 427);
+            this.remoteDesktop1.Location = new System.Drawing.Point(13, 13);
+            this.remoteDesktop1.Name = "remoteDesktop1";
+            this.remoteDesktop1.Size = new System.Drawing.Size(400, 200);
+            this.remoteDesktop1.TabIndex = 0;
+            this.remoteDesktop1.ViewOnly = false;
+            // 
+            // hostLabel
+            // 
+            this.hostLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.hostLabel.AutoSize = true;
+            this.hostLabel.Location = new System.Drawing.Point(13, 223);
+            this.hostLabel.Name = "hostLabel";
+            this.hostLabel.Size = new System.Drawing.Size(31, 13);
+            this.hostLabel.TabIndex = 1;
+            this.hostLabel.Text = "Хост";
+            // 
+            // portLabel
+            // 
+            this.portLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(13, 250);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(32, 13);
+            this.portLabel.TabIndex = 2;
+            this.portLabel.Text = "Порт";
+            // 
+            // hostTextBox
+            // 
+            this.hostTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.hostTextBox.Location = new System.Drawing.Point(55, 220);
+            this.hostTextBox.Name = "hostTextBox";
+            this.hostTextBox.Size = new System.Drawing.Size(353, 20);
+            this.hostTextBox.TabIndex = 3;
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.portTextBox.Location = new System.Drawing.Point(55, 247);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(353, 20);
+            this.portTextBox.TabIndex = 4;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectButton.Location = new System.Drawing.Point(218, 273);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(93, 23);
+            this.connectButton.TabIndex = 5;
+            this.connectButton.Text = "Подключиться";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.disconnectButton.Location = new System.Drawing.Point(317, 273);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(91, 23);
+            this.disconnectButton.TabIndex = 6;
+            this.disconnectButton.Text = "Отключиться";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // VncClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 303);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.hostTextBox);
+            this.Controls.Add(this.portLabel);
+            this.Controls.Add(this.hostLabel);
+            this.Controls.Add(this.remoteDesktop1);
+            this.Name = "VncClientForm";
+            this.Text = "VNC Клиент";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.VncClientForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private VncSharp.RemoteDesktop remoteDesktop1;
+        private System.Windows.Forms.Label hostLabel;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.TextBox hostTextBox;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button disconnectButton;
+    }
+}
+
---
+++ VncClient/VncClient/VncClient/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+using VncSharp;
+
+namespace VncClient
+{
+    public partial class VncClientForm : System.Windows.Forms.Form
+    {
+        public VncClientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            string remoteHost = hostTextBox.Text;
+            int port = Convert.ToInt32(portTextBox.Text);
+            try
+            {
+                remoteDesktop1.VncPort = port;
+                remoteDesktop1.Connect(remoteHost, false, true);
+            }
+            catch (VncProtocolException ex)
+            {
+                MessageBox.Show(string.Format("Невозможно установить соединение: {0}", ex.Message));
+            }
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                remoteDesktop1.Disconnect();
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(string.Format("Невозможно отключитья от сервера: {0}", ex.Message));
+            }
+        }
+
+        private void VncClientForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                remoteDesktop1.Disconnect();
+            }
+            catch (InvalidOperationException ex) { }
+        }
+    }
+}
